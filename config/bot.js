d4aa69c4c571ee6d416ff8cf6f7d35427353aeb6
KrisnaCahya <krisnacahyap2@gmail.com>
2021-02-17 14:02:54 +0700

    first commit

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODExNDc4MDEyNjE5OTE1MzM0.YCyxyg.LZgXT3REtD9Ywe4_nLxQVVTXE6c',
+        prefix: '=',
+        activity: 'K-Bot',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
